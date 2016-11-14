f04cd59954231db32bade2bb1c1130c0daa6757b
Henriquez <chenriquez@deloitte.com>
2016-11-13 20:53:47 -0500

    updated for mobile

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,22 @@
-console.log('\'Allo \'Allo!');
+// import JQuery from '../bower_components/jquery/dist/jquery.js';
+//var JQuery = require('../bower_components/jquery/dist/jquery.js');
+//var $ = require('../bower_components/jquery/dist/jquery.js');
+
+$('#about').on('click', function(e){
+  e.preventDefault();
+  $('html,body').animate({
+   scrollTop: $('.about-section').offset().top},'slow');  
+});
+
+
+$('#team').on('click', function(e){
+  e.preventDefault();
+  $('html,body').animate({
+   scrollTop: $('.team-section').offset().top},'slow');  
+});
+
+$('#contact').on('click', function(e){
+  e.preventDefault();
+  $('html,body').animate({
+   scrollTop: $('.team-section').offset().top},'slow');  
+});
